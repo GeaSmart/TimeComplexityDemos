2e03fbec4620b9551436f03b72b72760e03740f3
Gerson Azabache <gerson_dunk@hotmail.com>
2022-03-04 17:09:35 -0500

    linear executions in progress

--- TimeComplexityDemos/Program.cs
+++ TimeComplexityDemos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace TimeComplexityDemos
 {
@@ -6,7 +7,28 @@ namespace TimeComplexityDemos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Time complexity measurement:");
+            var arreglo1 = new int[] { 1, 2, 3 };
+            var arreglo2 = new int[] { 8, 6, 5, 4, 5, 6, 7, 8, 9, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 1, 1, 2, 3, 3, 4, 5 };
+
+            LinealExecution(arreglo1);
+            //LinealExecution(arreglo2);
+        }               
+
+        private static int LinealExecution(int[] arreglo)
+        {
+            DateTime start = DateTime.Now;
+
+            var contador = 0;
+            foreach(var item in arreglo)
+            {
+                contador++;
+            }            
+
+            TimeSpan elapsedTime = DateTime.Now - start;
+            Console.WriteLine($"Elapsed time: {elapsedTime.TotalMilliseconds}.");
+
+            return contador;
         }
     }
 }
