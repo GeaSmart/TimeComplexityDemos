d1cc3dc5796d9a0bea19fc77041502f66edb19fc
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2022-03-04 21:59:03 -0500

    linear

--- TimeComplexityDemos/Program.cs
+++ TimeComplexityDemos/Program.cs
@@ -8,25 +8,44 @@ namespace TimeComplexityDemos
         static void Main(string[] args)
         {
             Console.WriteLine("Time complexity measurement:");
-            var arreglo1 = new int[] { 1, 2, 3 };
-            var arreglo2 = new int[] { 8, 6, 5, 4, 5, 6, 7, 8, 9, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 1, 1, 2, 3, 3, 4, 5 };
+            int[] arreglo1 = populateArray(1000000);
+            int[] arreglo2 = populateArray(100000);
+            int[] arreglo3 = populateArray(10000);
+            int[] arreglo4 = populateArray(1000);
+            int[] arreglo5 = populateArray(100);
 
+            Console.WriteLine("--------- RESULTS ----------");
             LinealExecution(arreglo1);
-            //LinealExecution(arreglo2);
+            LinealExecution(arreglo2);
+            LinealExecution(arreglo3);
+            LinealExecution(arreglo4);
+            LinealExecution(arreglo5);
         }               
+
+        private static int[] populateArray(int length)
+        {
+            int[] array = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = i;
+            }
+
+            return array;
+        }
 
         private static int LinealExecution(int[] arreglo)
         {
             DateTime start = DateTime.Now;
 
             var contador = 0;
-            foreach(var item in arreglo)
+            for (int i = 0; i < arreglo.Length; i++)
             {
                 contador++;
-            }            
+            }
 
             TimeSpan elapsedTime = DateTime.Now - start;
-            Console.WriteLine($"Elapsed time: {elapsedTime.TotalMilliseconds}.");
+            Console.WriteLine($"Elapsed time: {elapsedTime.TotalMilliseconds}\t -> {arreglo.Length} iterations.");
 
             return contador;
         }
